622f8ac14a4ca58a0b4ca5aa6f797a2e299a2e5d
Myles Adams <myles.n.adams@gmail.com>
2018-10-04 03:08:07 -0700

    Fixed the first test

--- src/main/scala/introduction/UniversityStandard.scala
+++ src/main/scala/introduction/UniversityStandard.scala
@@ -3,7 +3,7 @@ package edu.ucsb.cs.cs162.tuts.introduction
 
 // A set of university standard constants.
 object UniversityStandard {
-  lazy val minimalCourseNumber: Int = 1
+  lazy val minimalCourseNumber: Int = 0
   lazy val maximalCourseNumber: Int = 600
 
   lazy val minimalSubjectLength: Int = 3
