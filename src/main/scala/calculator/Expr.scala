b91feac90e4c2ccac8b11ba85087c5d91e50f38a
Myles Adams <myles.n.adams@gmail.com>
2018-10-05 01:57:55 -0700

    Tutorial Part 2 complete

--- src/main/scala/calculator/Expr.scala
+++ src/main/scala/calculator/Expr.scala
@@ -19,9 +19,87 @@ final case class BinOp(op: String, left: Expr, right: Expr) extends Expr
 // The calculator object.
 object Calculator {
 
-  // Simplifies the head of the expression (should not simplify recursively!).  
-  def simplifyHead(expr: Expr): Expr = ???
-  
+  // Simplifies the head of the expression (should not simplify recursively!).
+  def simplifyHead(expr: Expr): Expr = {
+    expr match {
+      case UnOp("-", x) => x match {
+        case Var(name) => Var("-" + name)
+
+        case Num(value) => Num(-1 * value)
+
+        case UnOp("-", y) => y
+      }
+
+      case BinOp("+", Num(0), x) => x
+
+      case BinOp("+", x, Num(0)) => x
+
+      case BinOp("*", Num(1), x) => x
+
+      case BinOp("*", x, Num(1)) => x
+
+      case BinOp("*", Num(0), x) => Num(0)
+
+      case BinOp("*", x, Num(0)) => Num(0)
+
+      case BinOp("-", x, y) => Num((x == y).compare(true))
+
+      case BinOp(op, Num(x), Num(y)) => op match {
+        case "+" => Num(x + y)
+
+        case "-" => Num(x - y)
+
+        case "*" => Num(x * y)
+      }
+    }
+  }
+
   // Evaluates the expression to a numeric value.
-  def evaluate(expr: Expr): Double = ???
+  def evaluate(expr: Expr): Double = expr match {
+    case Num(value) => value
+
+    case Var(name) => 1
+
+    case UnOp(op, value) => op match {
+      case "-" => -evaluate(value)
+
+      case _ => 1
+    }
+
+    case BinOp(op, x, y) => {
+      if (x == Var("DUP")) {
+        op match {
+          case "-" => evaluate(y) - evaluate(y)
+
+          case "+" => evaluate(y) + evaluate(y)
+
+          case "*" => evaluate(y) * evaluate(y)
+
+          case _ => 1
+        }
+      }
+      else if (y == Var("DUP")) {
+        op match {
+          case "-" => evaluate(x) - evaluate(x)
+
+          case "+" => evaluate(x) + evaluate(x)
+
+          case "*" => evaluate(x) * evaluate(x)
+
+          case _ => 1
+        }
+      }
+      else {
+        op match {
+          case "-" => evaluate(x) - evaluate(y)
+
+          case "+" => evaluate(x) + evaluate(y)
+
+          case "*" => evaluate(x) * evaluate(y)
+
+          case _ => 1
+        }
+      }
+    }
+  }
 }
